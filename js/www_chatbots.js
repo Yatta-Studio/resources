baa8cc808d024b8781d0a703fd5c664c36ee22a8
thebentarrow <g.qt.truong@gmail.com>
2026-05-07 20:36:44 -0500

    www_chatbots update

--- js/www_chatbots.js
+++ js/www_chatbots.js
@@ -1,84 +1,82 @@
-if (typeof globalThis.www_chatbots === "undefined") {
-    Object.defineProperty(globalThis, 'www_chatbots', {
-        value: [
-            {
-                name: 'Google Gemini',
-                url: 'https://gemini.google.com/app',
-                features: []
-            },
-            {
-                name: 'xAI Grok',
-                url: 'https://grok.com/',
-                features: []
-            },
-            {
-                name: 'Anthropic Claude',
-                url: '',
-                features: []
-            },
-            {
-                name: 'OpenAI ChatGPT',
-                url: 'https://chatgpt.com/',
-                features: []
-            },
-            {
-                name: 'Allen AI',
-                url: 'https://playground.allenai.org/',
-                features: []
-            },
-            {
-                name: 'Mistral Le Chat',
-                url: 'https://chat.mistral.ai/chat',
-                features: []
-            },
-            {
-                name: 'Meta AI',
-                url: 'https://www.meta.ai/',
-                features: []
-            },
-            {
-                name: 'Alibaba Qwen',
-                url: 'https://chat.qwen.ai/',
-                features: []
-            },
-            {
-                name: 'Deepseek AI',
-                url: 'https://chat.deepseek.com/',
-                features: []
-            },
-            {
-                name: 'Moonshot AI Kimi',
-                url: 'https://www.kimi.com/',
-                features: []
-            },
-            {
-                name: 'Manus AI',
-                url: 'https://manus.im/app',
-                features: []
-            },
-            {
-                name: 'Z.ai',
-                url: 'https://z.ai/chat',
-                features: []
-            },
-            {
-                name: 'Perplexity AI',
-                url: 'https://www.perplexity.ai/',
-                features: []
-            },
-            {
-                name: 'Deep Cogito',
-                url: 'https://chat.deepcogito.com/',
-                features: []
-            },
-            {
-                name: 'HuggingChat',
-                url: 'https://huggingface.co/chat/',
-                features: []
-            }
-        ],
-        enumerable: true,
-        configurable: false,
-        writable: false,
-    });
-}
+Object.defineProperty(globalThis, 'www_chatbots', {
+    value: [
+        {
+            name: 'Google Gemini',
+            url: 'https://gemini.google.com/app',
+            features: []
+        },
+        {
+            name: 'xAI Grok',
+            url: 'https://grok.com/',
+            features: []
+        },
+        {
+            name: 'Anthropic Claude',
+            url: '',
+            features: []
+        },
+        {
+            name: 'OpenAI ChatGPT',
+            url: 'https://chatgpt.com/',
+            features: []
+        },
+        {
+            name: 'Allen AI',
+            url: 'https://playground.allenai.org/',
+            features: []
+        },
+        {
+            name: 'Mistral Le Chat',
+            url: 'https://chat.mistral.ai/chat',
+            features: []
+        },
+        {
+            name: 'Meta AI',
+            url: 'https://www.meta.ai/',
+            features: []
+        },
+        {
+            name: 'Alibaba Qwen',
+            url: 'https://chat.qwen.ai/',
+            features: []
+        },
+        {
+            name: 'Deepseek AI',
+            url: 'https://chat.deepseek.com/',
+            features: []
+        },
+        {
+            name: 'Moonshot AI Kimi',
+            url: 'https://www.kimi.com/',
+            features: []
+        },
+        {
+            name: 'Manus AI',
+            url: 'https://manus.im/app',
+            features: []
+        },
+        {
+            name: 'Z.ai',
+            url: 'https://z.ai/chat',
+            features: []
+        },
+        {
+            name: 'Perplexity AI',
+            url: 'https://www.perplexity.ai/',
+            features: []
+        },
+        {
+            name: 'Deep Cogito',
+            url: 'https://chat.deepcogito.com/',
+            features: []
+        },
+        {
+            name: 'HuggingChat',
+            url: 'https://huggingface.co/chat/',
+            features: []
+        }
+    ],
+    enumerable: true,
+    configurable: false,
+    writable: false,
+});
